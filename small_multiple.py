7655feb553ca59f01cdde4e0d9915f534db44c2b
Mohammadali <63012627+moa-coder@users.noreply.github.com>
2020-12-13 15:36:54 +0330

    Add files via upload

---
+++ small_multiple.py
@@ -0,0 +1,19 @@
+p, d = input().split()
+p = int(p)
+d = int(d) 
+
+# for help to find algoritm ------------------------------------
+#print (p)
+#print(d)
+
+#print()
+#first_step = d % p
+#print (d, first_step)
+#seconde_step = (first_step * 2) % p
+#print (d * 2, seconde_step)
+#third_step = (first_step * 3) % p
+#print (d * 3,third_step)
+#fourth_step = (first_step * 4) % p
+#print (d * 4,fourth_step)
+
+# -------------------------------------------
